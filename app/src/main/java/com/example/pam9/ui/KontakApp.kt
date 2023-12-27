d939d502769303b12b243d6b68b8f6d7003bda36
Devina02 <devinafitriapngs@gmail.com>
2023-12-27 21:31:02 +0700

    memperbaiki code yang salah dan merubah code pada navigationIcon

--- app/src/main/java/com/example/pam9/ui/KontakApp.kt
+++ app/src/main/java/com/example/pam9/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.pam9.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -42,15 +46,25 @@ fun KontakApp(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBarKontak(title: String, canNavigateBack: Modifier = Modifier, scrollBehavior: TopAppBarScrollBehavior? = null, navigateUp: () -> Unit = {}) {
-    CenterAlignedTopAppBar(
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar (title = { Text(title) } ,
+        modifier = modifier,
         scrollBehavior = scrollBehavior,
-        title = {
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineSmall,
-           )
-        },
-        modifier = modifier
-    )
-}
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                }
+
+                }
+            }
+        }
+
+
+
