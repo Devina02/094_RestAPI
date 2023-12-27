7f6c115b71c733cbe9830730368eb371be4b589f
Devina02 <devinafitriapngs@gmail.com>
2023-12-28 00:25:14 +0700

    membuat scaffold

--- app/src/main/java/com/example/pam9/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/pam9/ui/home/screen/HomeScreen.kt
@@ -21,10 +21,13 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -33,6 +36,7 @@ import com.example.pam9.R
 import com.example.pam9.model.Kontak
 import com.example.pam9.navigation.DestinasiNavigasi
 import com.example.pam9.ui.PenyediaViewModel
+import com.example.pam9.ui.TopAppBarKontak
 import com.example.pam9.ui.home.viewmodel.HomeViewModel
 import com.example.pam9.ui.home.viewmodel.KontakUIState
 
@@ -49,7 +53,17 @@ fun HomeScreen(
     onDetailClick: (Int) -> Unit = {},
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title = DestinasiHome.titleRes ,
+                canNavigasiBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        }
 }
 
 @Composable
