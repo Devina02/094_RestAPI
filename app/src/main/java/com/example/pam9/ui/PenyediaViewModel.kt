605ece7043af4695fa4f8c15a86eabdb2f275fb7
Devina02 <devinafitriapngs@gmail.com>
2023-12-28 00:46:50 +0700

    menambahkan InsertViewModel kedalam PenyediaViewModel

--- app/src/main/java/com/example/pam9/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pam9/ui/PenyediaViewModel.kt
@@ -1,17 +1,21 @@
 package com.example.pam9.ui
 
-import android.text.Spannable.Factory
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pam9.KontakAplikation
 import com.example.pam9.ui.home.viewmodel.HomeViewModel
+import com.example.pam9.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
+
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
