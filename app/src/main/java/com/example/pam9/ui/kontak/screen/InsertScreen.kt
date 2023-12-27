1add305c12d4849966cba28d80909e611210f753
Devina02 <devinafitriapngs@gmail.com>
2023-12-28 00:53:05 +0700

    membuat fun FormInputSiswa

--- app/src/main/java/com/example/pam9/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/pam9/ui/kontak/screen/InsertScreen.kt
@@ -1,20 +1,27 @@
 package com.example.pam9.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam9.navigation.DestinasiNavigasi
 import com.example.pam9.ui.PenyediaViewModel
 import com.example.pam9.ui.TopAppBarKontak
+import com.example.pam9.ui.kontak.viewmodel.InsertUiEvent
 import com.example.pam9.ui.kontak.viewmodel.InsertViewModel
 import kotlinx.coroutines.launch
 
@@ -56,6 +63,56 @@ fun EntryKontakScreen(
                 .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
                 .fillMaxWidth()
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange = {onValueChange(insertUiEvent.copy(alamat = it))},
+            label = { Text("Email")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.telpon,
+            onValueChange = {onValueChange(insertUiEvent.copy(telpon = it))},
+            label = { Text("No Hp")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if (enabled){
+            Text(
+                text = "Isi semua data",
+                modifier = Modifier.padding(start = 12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
         )
     }
 }
