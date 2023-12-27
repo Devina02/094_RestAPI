2634c8cd36921c5f5a17d86c9bc15aa79088f00a
Devina02 <devinafitriapngs@gmail.com>
2023-12-28 00:41:43 +0700

    membuat data class InsertUiState

--- app/src/main/java/com/example/pam9/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam9/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,8 +1,32 @@
 package com.example.pam9.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pam9.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 
 class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try{
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
