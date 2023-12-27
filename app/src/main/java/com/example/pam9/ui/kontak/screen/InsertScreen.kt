7fae44f3d48d66d74ef0e5beea575e89ca29ef99
Devina02 <devinafitriapngs@gmail.com>
2023-12-28 00:48:33 +0700

    membuat object DestinasiEntry

---
+++ app/src/main/java/com/example/pam9/ui/kontak/screen/InsertScreen.kt
@@ -0,0 +1,8 @@
+package com.example.pam9.ui.kontak.screen
+
+import com.example.pam9.navigation.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
